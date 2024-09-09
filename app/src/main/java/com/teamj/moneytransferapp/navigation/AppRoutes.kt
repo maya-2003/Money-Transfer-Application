5f3755df8e8accd114fb8828abd3bed8cbf050b5
infinitix021 <omarmadian012@yahoo.com>
2024-09-09 15:50:43 +0300

    Phase 1 of Transfer Screen

--- app/src/main/java/com/teamj/moneytransferapp/navigation/AppRoutes.kt
+++ app/src/main/java/com/teamj/moneytransferapp/navigation/AppRoutes.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.teamj.moneytransferapp.transfer.TransferScreen
 
 object  Route{
     const val SIGNUP = "signup"
@@ -18,10 +19,14 @@ object  Route{
     const val NOTIFICATIONS = "notifications"
     const val LAST_TRANSACTIONS = "last_transactions"
     const val TRANSACTION = "transaction"
+    const val TRANSFER_PH1 = "transfer_ph1"
+
 }
 @Composable
 fun AppNavHost(onSendNotification: () -> Unit,modifier: Modifier = Modifier){
     val navController= rememberNavController()
-    NavHost(navController = navController, startDestination = Route.SIGNUP) {
+    NavHost(navController = navController, startDestination = Route.TRANSFER_PH1) {
+        composable(route = Route.TRANSFER_PH1){ TransferScreen(navController) }
+
     }
 }
