070b3da0f910258bcc765d01f195ed577cd84d6e
maya-2003 <maya.elb2003@gmail.com>
2024-09-09 23:17:49 +0300

    Merge Phase1

--- app/src/main/java/com/teamj/moneytransferapp/navigation/AppRoutes.kt
+++ app/src/main/java/com/teamj/moneytransferapp/navigation/AppRoutes.kt
@@ -5,12 +5,15 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.teamj.moneytransferapp.transfer.TransferScreen
 import com.teamj.moneytransferapp.ui.screens.AddCardScreen
 import com.teamj.moneytransferapp.ui.screens.CardAddedScreen
 import com.teamj.moneytransferapp.ui.screens.CardSplashScreen
+import com.teamj.moneytransferapp.ui.screens.CompleteProfileScreen
 import com.teamj.moneytransferapp.ui.screens.OTPScreenBar
+import com.teamj.moneytransferapp.ui.screens.SignupScreen
 
-object  Route{
+object Route {
     const val SIGNUP = "signup"
     const val CONTINUE_SIGNUP = "continue_signup"
     const val PROFILE = "profile"
@@ -26,24 +29,20 @@ object  Route{
     const val CARD_SPLASH = "card_splash"
     const val CARD_OTP = "card_otp"
     const val CARD_ADDED = "card_added"
-
     const val TRANSFER_PH1 = "transfer_ph1"
 
 }
+
 @Composable
-fun AppNavHost(onSendNotification: () -> Unit,modifier: Modifier = Modifier){
-    val navController= rememberNavController()
+fun AppNavHost(onSendNotification: () -> Unit, modifier: Modifier = Modifier) {
+    val navController = rememberNavController()
     NavHost(navController = navController, startDestination = Route.ADD_CARD) {
-        composable(route = Route.ADD_CARD){ AddCardScreen(navController) }
-        composable(route = Route.CARD_SPLASH){ CardSplashScreen(navController) }
-        composable(route = Route.CARD_OTP){ OTPScreenBar(navController) }
-        composable(route = Route.CARD_ADDED){ CardAddedScreen(navController) }
-        composable(Route.SIGNUP) {
-            SignupScreen(navController)
-        }
-        composable(Route.CONTINUE_SIGNUP) {
-            CompleteProfileScreen(navController)
-        }
+        composable(route = Route.ADD_CARD) { AddCardScreen(navController) }
+        composable(route = Route.CARD_SPLASH) { CardSplashScreen(navController) }
+        composable(route = Route.CARD_OTP) { OTPScreenBar(navController) }
+        composable(route = Route.CARD_ADDED) { CardAddedScreen(navController) }
+        composable(Route.SIGNUP) { SignupScreen(navController) }
+        composable(Route.CONTINUE_SIGNUP) { CompleteProfileScreen(navController) }
         composable(route = Route.TRANSFER_PH1){ TransferScreen(navController) }
     }
 }
