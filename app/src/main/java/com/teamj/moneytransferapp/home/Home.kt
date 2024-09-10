1b87f22613329b85d6462c06b0827975473010b7
maya-2003 <maya.elb2003@gmail.com>
2024-09-10 09:14:56 +0300

    Update Home

--- app/src/main/java/com/teamj/moneytransferapp/home/Home.kt
+++ app/src/main/java/com/teamj/moneytransferapp/home/Home.kt
@@ -17,19 +17,12 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -40,7 +33,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -49,11 +41,8 @@ import androidx.navigation.compose.rememberNavController
 import com.maya.moneytransferapp.Transaction
 import com.maya.moneytransferapp.TransactionCard
 import com.teamj.moneytransferapp.R
-import com.teamj.moneytransferapp.common.navBottomBar
+import com.teamj.moneytransferapp.common.NavBottomBar
 import com.teamj.moneytransferapp.data.DataSource
-import com.teamj.moneytransferapp.more.HelpBottomSheet
-import com.teamj.moneytransferapp.more.MoreOptions
-import com.teamj.moneytransferapp.navigation.Route
 import com.teamj.moneytransferapp.ui.theme.G0
 import com.teamj.moneytransferapp.ui.theme.G100
 import com.teamj.moneytransferapp.ui.theme.G200
@@ -69,7 +58,7 @@ import com.teamj.moneytransferapp.ui.theme.YellowGrad
 fun HomeScreen(navController: NavController, modifier: Modifier = Modifier) {
     Scaffold(
         bottomBar = {
-            navBottomBar(state = 1, navController = navController)
+            NavBottomBar(state = 1, navController = navController)
         },
         containerColor = Color.Transparent
     ) { innerPadding ->
